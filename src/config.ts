0d26d55d3938d106ffbb5176048627dfc47616dd
Francis Phan <francis.nicholasphan@gmail.com>
2024-01-21 20:21:26 +1300

    Reformat thumbnail

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  website: "https://phan.nz/", // replace this with your deployed domain
+  author: "Francis Phan",
+  desc: "A blog about software engineering, Cloud, Android, and other things I find interesting.",
   title: "Francis Phan",
-  ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
